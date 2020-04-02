4af9effc8a20d8a478c6c79dfa59f736fa1f5ff4
SAHELI  BASU <sahelirima23@gmail.com>
2020-04-02 15:16:25 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,110 @@
+package com.myappcompany.nick.connect3game;
+
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //0: yellow, 1: red, 2: empty
+
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+
+    int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+
+    int activePlayer = 0;
+    int score = 0;
+    boolean gameActive = true;
+
+    public void dropIn(View view)
+    {
+        ImageView counter = (ImageView) view;
+
+        //Getting the tag as a string from the object and parsing it to an int to later be stored in the array
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
+
+        if(gameState[tappedCounter] == 2 && gameActive)
+        {
+
+            //Setting each element in the array to it's corresponding coloured game object
+            gameState[tappedCounter] = activePlayer;
+
+            counter.setTranslationY(-1500);
+
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.yellow);
+                score++;
+                activePlayer = 1;
+            } else {
+                counter.setImageResource((R.drawable.red));
+                score++;
+                activePlayer = 0;
+            }
+
+            counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
+
+            for (int[] winningPosition : winningPositions) {
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2)
+                {
+
+                    gameActive = false;
+                    String winner = "";
+
+                    if (activePlayer == 1) {
+                        winner = "Yellow";
+                    } else {
+                        winner = "Red";
+                    }
+
+                    Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
+                    TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
+                    winnerTextView.setText(winner + " has Won!");
+                    playAgainButton.setVisibility(view.VISIBLE);
+                    winnerTextView.setVisibility(view.VISIBLE);
+                }
+                else if(score == 9)
+                {
+                    Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
+                    TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
+                    winnerTextView.setText("Tie Game!");
+                    playAgainButton.setVisibility(view.VISIBLE);
+                    winnerTextView.setVisibility(view.VISIBLE);
+                }
+            }
+        }
+    }
+
+    public void playAgain(View view)
+    {
+        Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
+        TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
+        playAgainButton.setVisibility(view.INVISIBLE);
+        winnerTextView.setVisibility(view.INVISIBLE);
+
+        android.support.v7.widget.GridLayout gridLayout = findViewById(R.id.gridLayout);
+        for (int i = 0; i < gridLayout.getChildCount(); i++)
+        {
+            ImageView counter = (ImageView) gridLayout.getChildAt(i);
+            counter.setImageDrawable(null);
+        }
+
+        for(int i = 0; i < gameState.length; i++)
+        {
+            gameState[i] = 2;
+        }
+        activePlayer = 0;
+        gameActive = true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
